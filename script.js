056a468f236409ccb0b93527aa1fb77d7c1507ab
sn2368 <shreyasnair.2368@gmail.com>
2025-04-24 02:33:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,33 +28,3 @@ const solutionObserver = new IntersectionObserver((entries) => {
 });
 
 document.querySelectorAll('.solution-heading, .pod').forEach(el => solutionObserver.observe(el));
-
- function setActiveLink() {
-    const links = document.querySelectorAll('.nav-link');
-    const sections = document.querySelectorAll('section');
-    
-    let currentSection = '';
-    
-    sections.forEach(section => {
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.offsetHeight;
-      const scrollPosition = window.scrollY;
-
-      if (scrollPosition >= sectionTop - 50 && scrollPosition < sectionTop + sectionHeight) {
-        currentSection = section.getAttribute('id');
-      }
-    });
-    
-    links.forEach(link => {
-      link.classList.remove('active');
-      if (link.getAttribute('href').includes(currentSection)) {
-        link.classList.add('active');
-      }
-    });
-  }
-
-  // Run the function on scroll
-  window.addEventListener('scroll', setActiveLink);
-  
-  // Run the function on load in case the page is reloaded
-  window.addEventListener('load', setActiveLink);
